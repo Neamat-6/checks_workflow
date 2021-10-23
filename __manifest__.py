809617dd1b6b7d680821b1dee4bb8f14791d05d3
Neamat-6 <neamatgamalsalem@gmail.com>
2021-10-23 22:58:46 +0200

    third

--- __manifest__.py
+++ __manifest__.py
@@ -8,8 +8,8 @@
         'security/ir.model.access.csv',
         'wizard/cancel_reason_wizard.xml',
         'views/account_journal_inherit_view.xml',
-        # 'views/account_payment_inherit_view.xml',
-        # 'views/account_payment_actions.xml',
-        # 'views/menu_item_view.xml',
+        'views/account_payment_inherit_view.xml',
+        'views/account_payment_actions.xml',
+        'views/menu_item_view.xml',
     ],
 }
